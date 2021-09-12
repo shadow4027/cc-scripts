dbc0f50d4375d2a3bcfca31bbc039782847b6f5d
= <jbloom520@gmail.com>
2021-09-11 22:27:36 -0600

    Added the template and first script

---
+++ user_scripts/pando_search_five9_number_search.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Pando-five9-number-search
+// @namespace    alder_util
+// @version      0.1
+// @description  Grabes the phone number from the five9 script and they searches it. 
+// @author       Jeremy Bloom
+// @homepageURL
+// @match        https://alder.pandolink.com/customer/search
+// @icon         https://www.google.com/s2/favicons?domain=alder.pando.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log("hello wworld");
+    
+})();
---
+++ userscript_template.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Template
+// @namespace    <namespace>
+// @version      0.0
+// @description  <Insert Desc>
+// @author       Jeremy Bloom
+// @match        <Insert URI here>
+// @icon         https://www.google.com/s2/favicons?domain=<insert domain name here>
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+
+})();
