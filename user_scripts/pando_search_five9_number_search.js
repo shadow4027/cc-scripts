75a82d3adf2a2cfdfc0f19515439b956394fce88
= <jbloom520@gmail.com>
2021-09-11 22:37:47 -0600

    updated the headers for pando_search_five0_search.js

--- user_scripts/pando_search_five9_number_search.js
+++ user_scripts/pando_search_five9_number_search.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name         Pando-five9-number-search
 // @namespace    alder_util
-// @version      0.1
-// @description  Grabes the phone number from the five9 script and they searches it. 
+// @version      0.2
+// @description  Grabes the phone number from the five9 script and then searches it. 
 // @author       Jeremy Bloom
-// @homepageURL
+// @updateURL    https://github.com/shadow4027/cc-scripts/blob/main/user_scripts/pando_search_five9_number_search.js?raw=true   
+// @downloadURL  https://github.com/shadow4027/cc-scripts/blob/main/user_scripts/pando_search_five9_number_search.js?raw=true
+// @homepageURL  https://github.com/shadow4027
 // @match        https://alder.pandolink.com/customer/search
 // @icon         https://www.google.com/s2/favicons?domain=alder.pando.com
 // @grant        none
